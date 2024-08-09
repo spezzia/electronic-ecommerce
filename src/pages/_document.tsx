14d0ac6c5c15aa9ebf6dd68d5bfc2932705053f8
spezzia <50023671+spezzia@users.noreply.github.com>
2024-08-08 18:15:40 -0600

    feat(): Se agregaron las metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <title>Electronic ecommerce</title>
+        <meta
+          name='description'
+          content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+        ></meta>
+        <meta name='keywords' content='key1, key2, key3'></meta>
+        <meta name='author' content='Jorge Luis Bello'></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
